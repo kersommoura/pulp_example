9db4f86569b67bb3a816b121f6bfb986e90e8f86
werwty <bihan.zh@gmail.com>
2017-09-25 09:26:50 -0400

    Fix minor pypi README rendering issues. Prepare package for alpha1 release.

--- setup.py
+++ setup.py
@@ -1,21 +1,25 @@
 #!/usr/bin/env python3
 
-from setuptools import setup
+from setuptools import setup, find_packages
 
 requirements = [
     'pulpcore-plugin',
 ]
 
+with open('README.rst') as f:
+    long_description = f.read()
+
 setup(
     name='pulp-example',
-    version='0.0.1a1.dev1',
+    version='0.0.1a1',
     description='Example plugin for the Pulp Project',
+    long_description=long_description,
     author='Pulp Project Developers',
     author_email='pulp-dev@redhat.com',
     url='http://www.pulpproject.org/',
     install_requires=requirements,
     include_package_data=True,
-    packages=['pulp_example', 'pulp_example.app'],
+    packages=find_packages(exclude=['test']),
     entry_points={
         'pulpcore.plugin': [
             'pulp_example = pulp_example:default_app_config',
